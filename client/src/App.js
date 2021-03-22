d6a26aac5030b8d6082623eb877b4ea5049b5aeb
mohanmohadikar <mohadikar.mohan@sitpune.edu.in>
2021-03-23 01:14:55 +0530

    initial commit.

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import Appbar from './components/Appbar/Appbar.js';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Appbar />
+      <h1>Hello world!!!</h1>
+    </div>
+  );
+}
+
+export default App;
